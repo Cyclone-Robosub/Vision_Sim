60f27ef7a413496717f1706e980151f3d5bd6923
Liam Multhaup <liam@jmulthaup.com>
2026-01-27 17:35:07 -0800

    Simulink Unreal Plugin Tutorial Upload

---
+++ Unreal/UnrealProjects/SimulinkUnrealPluginTutorial/UnrealEnvironment/Source/UnrealEnvironment/UnrealEnvironment.Build.cs
@@ -0,0 +1,23 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class UnrealEnvironment : ModuleRules
+{
+	public UnrealEnvironment(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "MathWorksSimulation" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
