da344a6b190291bb7742b5b24a8d43508611cd95
Liam Multhaup <liam@jmulthaup.com>
2026-04-04 16:09:47 -0700

    Refresh

--- Unreal/UnrealProjects/SimulinkCoSimulation/getWorldToImage.m
+++ Unreal/UnrealProjects/SimulinkCoSimulation/getWorldToImage.m
@@ -1,24 +1,43 @@
-function position = getWorldToImage(r4, cameraPos)
-    position = r;
+
+function r = getWorldToImage(p, cameraPos, stereoParams)
+    cameraExt = getCameraExtrinsic(cameraPos);
+    k = stereoParams.CameraParameters1.K;
+    cameraInt = zeros(4,4);
+    cameraInt(1:3, 1:3) = k(1:3, 1:3);
+    cameraMatrix = cameraInt*cameraExt;
+    p = transpose(p);
+    rUnDistorted = cameraMatrix * p;
+    r = rUnDistorted;
 end
 
 
-function cExt = getCameraExtrinsic(cameraPos)
+function cameraExt = getCameraExtrinsic(cameraPos)
+
+    %Roll Matrix:
     RM = getRollMatrix(cameraPos(4));
+
+    %Pitch Matrix:
     PM = getPitchMatrix(cameraPos(5));
+
+    %Yaw Matrix:
     YM = getYawMatrix(cameraPos(6));
-    translationM = [1 0 0 -cameraPos(1); 0 1 0 -cameraPos(2); 0 0 1 -cameraPos(3); 0 0 0 1];
-    rotationM = rotationM;
+
+    %Translation Matrix:
+    TM = [1 0 0 -cameraPos(1); 0 1 0 -cameraPos(2); 0 0 1 -cameraPos(3); 0 0 0 1];
+
+    %Camera External Matrix Computed in Translation => Yaw => Pitch => Roll
+    %order:
+    cameraExt = RM*PM*YM*TM;
 end
 
 function rollM = getRollMatrix(roll)
-    rollM = [1 0 0; 0 cos(roll) -sin(roll); 0 sin(roll) cos(roll)];
+    rollM = [1 0 0 0; 0 cos(roll) -sin(roll) 0; 0 sin(roll) cos(roll) 0; 0 0 0 1];
 end
 
 function pitchM = getPitchMatrix(pitch)
-    pitchM = [cos(pitch) 0 sin(pitch); 0 1 0; -sin(pitch) 0 cos(pitch)];
+    pitchM = [cos(pitch) 0 sin(pitch) 0; 0 1 0 0; -sin(pitch) 0 cos(pitch) 0; 0 0 0 1];
 end
 
 function yawM = getYawMatrix(yaw)
-    yawM = [cos(yaw) -sin(yaw) 0; sin(yaw) cos(yaw) 0; 0 0 1];
+    yawM = [cos(yaw) -sin(yaw) 0 0; sin(yaw) cos(yaw) 0 0; 0 0 1 0; 0 0 0 1];
 end
